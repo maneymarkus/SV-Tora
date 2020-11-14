cf83947c5ceb10cf6b1d47f6cc125b797893222d
maneymarkus <markus.staedler@hotmail.de>
2020-11-15 00:29:05 +0100

    Finished tooltips

--- Frontend/tooltip/js/tooltip.js
+++ Frontend/tooltip/js/tooltip.js
@@ -1,6 +1,11 @@
 function init(window, document, undefined) {
 
-
+  tippy(".tt-trigger", {
+    content: "My tooltip!",
+    offset: [0, 20],
+    arrow: tippy.roundArrow,
+    theme: "svtora",
+  });
 
 }
 
