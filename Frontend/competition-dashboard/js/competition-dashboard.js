1bab2f7989e68708e8f126b6421f364609d4e8f0
maneymarkus <markus.staedler@hotmail.de>
2020-11-16 19:48:38 +0100

    Finished competition dashboard

--- Frontend/competition-dashboard/js/competition-dashboard.js
+++ Frontend/competition-dashboard/js/competition-dashboard.js
@@ -1,5 +1,35 @@
 function init(window, document, undefined) {
-  const tilt = $('.host-tournament').tilt({maxTilt: 15, scale: 1.1});
+
+  let progressSlider = document.querySelectorAll("div.progress-container");
+  progressSlider.forEach((ps) => {
+    let currentProgress = ps.getAttribute("data-progress");
+    ps.querySelector(".progress").style.width = currentProgress + "%";
+    ps.querySelector(".handle").style.left = currentProgress + "%";
+  });
+
+  let progressSteps = document.querySelectorAll("div.step-container");
+  progressSteps.forEach((pStep) => {
+    let currentProgress = parseInt(pStep.getAttribute("data-step"));
+    let steps = pStep.querySelectorAll("p.step");
+    let counter = 0;
+    steps.forEach((step) => {
+      if (counter === currentProgress) {
+        step.classList.add("current");
+      } else if (counter < currentProgress) {
+        step.classList.add("done");
+      }
+      counter++;
+    });
+  });
+
+  /* TODO: Activate
+  let tiltOptions = {maxTilt: 15, scale: 1.15};
+  const primaryButtonTilt = $(".primary-button").tilt(tiltOptions);
+
+  $('.primary-button').click(function (e){
+    e.preventDefault();
+  });
+   */
 }
 
 window.addEventListener("DOMContentLoaded", function() {
