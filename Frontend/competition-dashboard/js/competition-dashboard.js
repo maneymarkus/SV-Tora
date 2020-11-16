fb60eff10f998dc2623d8971ec964fef3ead8495
maneymarkus <markus.staedler@hotmail.de>
2020-11-16 19:47:57 +0100

    Finished competition dashboard

---
+++ Frontend/competition-dashboard/js/competition-dashboard.js
@@ -0,0 +1,7 @@
+function init(window, document, undefined) {
+  const tilt = $('.host-tournament').tilt({maxTilt: 15, scale: 1.1});
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+  init(window, document);
+});
